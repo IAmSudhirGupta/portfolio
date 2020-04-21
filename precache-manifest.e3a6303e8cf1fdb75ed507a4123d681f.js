718d034c8d2776b01541e3627e7af0978ce7e94d
Sudhir Gupta <sudhirhitit@gmail.com>
2020-04-21 14:55:36 +0530

    Updates

---
+++ precache-manifest.e3a6303e8cf1fdb75ed507a4123d681f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a88245ae9dccead515fd5e725e6fa48",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "58ee31466d6c41c59124",
+    "url": "/portfolio/static/css/main.03fb61ff.chunk.css"
+  },
+  {
+    "revision": "bc767885de269b0fbc1d",
+    "url": "/portfolio/static/js/2.ac06180e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ac06180e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ee31466d6c41c59124",
+    "url": "/portfolio/static/js/main.63bc0a58.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "1180533cb0c3a95b7a7641940c93950f",
+    "url": "/portfolio/static/media/profile.1180533c.png"
+  }
+]);
